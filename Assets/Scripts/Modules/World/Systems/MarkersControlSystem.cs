68ab5ccc2c3bd3e9952acd9216bfb5730ccd598f
BoogiemanNSK <calah47@yandex.ru>
2020-02-02 10:21:07 +0300

    Rings hot fix

--- Assets/Scripts/Modules/World/Systems/MarkersControlSystem.cs
+++ Assets/Scripts/Modules/World/Systems/MarkersControlSystem.cs
@@ -6,6 +6,8 @@ namespace Modules.CoreGame
     {
         // Auto-injected
         private EcsFilter<PointOwner, MarkerRoot> _filter;
+        private EcsFilter<MarkerRoot>.Exclude<PointOwner> _neutralFilter;
+
         readonly PlayerApi _playerApi;
 
         public MarkersControlSystem(PlayerApi playerApi) {
@@ -16,14 +18,17 @@ namespace Modules.CoreGame
         {
             foreach (var i in _filter)
             {
-                _filter.Get2[i].FriendlyMarker.SetActive(false);
-                _filter.Get2[i].EnemyMarker.SetActive(false);
-
                 if (_filter.Get1[i].OwnerID == _playerApi.PlayerName) {
                     _filter.Get2[i].FriendlyMarker.SetActive(true);
+                    _filter.Get2[i].EnemyMarker.SetActive(false);
                 } else if (_filter.Get1[i].OwnerID != "") {
                     _filter.Get2[i].EnemyMarker.SetActive(true);
+                    _filter.Get2[i].FriendlyMarker.SetActive(false);
                 }
+            }
+            foreach (var i in _neutralFilter) {
+                _neutralFilter.Get1[i].FriendlyMarker.SetActive(false);
+                _neutralFilter.Get1[i].EnemyMarker.SetActive(false);
             }
         }
     }
